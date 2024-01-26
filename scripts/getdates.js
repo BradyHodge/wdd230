01855dac7e964bb2fc10ae189580e6a0a4077f03
BradyHodge <bradyhodge1@gmail.com>
2024-01-26 12:58:41 -0700

    added hamburger

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,10 @@ document.getElementById("copyYear").textContent = currentYear;
 let lastMod = new Date(document.lastModified);
 document.getElementById("lastModified").textContent = `Last Updated: ${lastMod}`;
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('active');
+	hamButton.classList.toggle('open');
+});
