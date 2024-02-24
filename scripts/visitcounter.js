0a6fc9040cfed4a7f1d8b35b62e8c4d376a0af00
bradyhodge <bradyhodge1@gmail.com>
2024-02-24 13:10:53 -0700

    added visit counter

---
+++ scripts/visitcounter.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('visits')) {
+        localStorage.setItem('visits', parseInt(localStorage.getItem('visits')) + 1);
+    } else {
+        localStorage.setItem('visits', 1);
+    }
+    document.getElementById('page-visits').textContent = localStorage.getItem('visits');
+});
