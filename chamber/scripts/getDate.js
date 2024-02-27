4a3f7c09bf29d933f522eab84803bc5f7918ef7f
bradyhodge <bradyhodge1@gmail.com>
2024-02-26 21:08:12 -0700

    Update chamber website styles and scripts

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -1,8 +1,5 @@
-// get current year for copy
 let currentYear = new Date().getFullYear();
 document.getElementById("copyYear").textContent = currentYear;
-
-// get last modified date
 let lastMod = new Date(document.lastModified);
 document.getElementById("lastModified").textContent = `Last Updated: ${lastMod}`;
 
