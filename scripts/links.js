1999284a0af52dca3f008a183b1fb791befe4c62
bradyhodge <bradyhodge1@gmail.com>
2024-03-07 21:20:19 -0700

    Finish implementing the JSON links

---
+++ scripts/links.js
@@ -0,0 +1,27 @@
+const baseURL = "https://bradyhodge.github.io/wdd230/";
+const linksURL = "https://bradyhodge.github.io/wdd230/data/links.json";
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+function displayLinks(weeks) {
+    const container = document.getElementById('activities');
+    weeks.lessons.forEach(lesson => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Lesson ${lesson.lesson}:`;
+        const linksList = document.createElement('ul');
+        lesson.links.forEach(link => {
+            const linkItem = document.createElement('li');
+            const linkAnchor = document.createElement('a');
+            linkAnchor.textContent = link.title;
+            linkAnchor.href = baseURL + link.url;
+            linkAnchor.target = '_blank'; // Open in a new tab
+            linkItem.appendChild(linkAnchor);
+            linksList.appendChild(linkItem);
+        });
+        listItem.appendChild(linksList);
+        container.appendChild(listItem);
+    });
+}
+getLinks();
